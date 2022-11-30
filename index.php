8c77092bf1a9cc3878b4fe549bc3570aa343d551
saracassandra7 <saracassandra7@gmail.com>
2022-11-30 17:34:22 +0100

    [add] a bit of style

--- index.php
+++ index.php
@@ -5,10 +5,18 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Badwords</title>
+  <style>
+    input{
+      width: 200px;
+      height: 20px;
+      margin: 10px 0;
+    }
+  </style>
+
 </head>
 <body>
   <form action="./pagina-di-atterraggio.php" method="POST">
-  <textarea name="paragraph" cols="30" rows="10" placeholder="Inserisci un paragrafo"></textarea>
+  <textarea name="paragraph" cols="40" rows="5" placeholder="Inserisci un paragrafo"></textarea>
   <br>
   <input type="text" name="badword" placeholder="Inserisci la parola da censurare">
   <br>
--- pagina-di-atterraggio.php
+++ pagina-di-atterraggio.php
@@ -12,13 +12,30 @@ $paragrafo_censurato = str_replace($parola,'***',$paragrafo);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Badwords</title>
+
+  <style>
+    body{
+      font-family: Arial;
+      margin-left: 20px;
+    }
+
+    h3:nth-of-type(2){
+      margin-top: 60px;
+    }
+    
+    p{
+      color: blue;
+    }
+  </style>
 </head>
 <body>
-  <h3>Il tuo paragrafo, che è lungo <?php echo strlen($paragrafo) ?> caratteri </h3>
+  <h3>Il tuo paragrafo </h3>
   <p><?php echo $paragrafo ?></p>
+  <h5>Lunghezza paragrafo: <?php echo strlen($paragrafo) ?> caratteri</h5>
 
-  <h3>Il tuo paragrafo censurato, che è lungo <?php echo strlen($paragrafo_censurato) ?> caratteri</h3>
+
+  <h3>Il tuo paragrafo censurato </h3>
   <p><?php echo $paragrafo_censurato ?></p>
-  
+  <h5>Lunghezza paragrafo: <?php echo strlen($paragrafo_censurato) ?> caratteri</h5>
 </body>
 </html>
